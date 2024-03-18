db15140cb359633c2dbf222d8dca45939d9dbae2
Manish Bhamare <41476046+ManishBB@users.noreply.github.com>
2024-03-18 19:49:21 +0530

    Updated api

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const conf = {
     serverUrl: String(import.meta.env.SERVER_URL),
-    baseUrl: "http://localhost:8000/api/v1",
+    baseUrl: "https://nice-ruby-chinchilla-hat.cyclic.app/api/v1",
     emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
 };
 
